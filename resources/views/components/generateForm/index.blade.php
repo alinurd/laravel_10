bb6d2666300906c7316787030f25a2b30fbcb4d1
ali dev14 <ali.nrdn14005@gmail.com>
2024-11-10 09:05:12 +0700

    started generate form

--- resources/views/components/generateForm/index.blade.php
+++ resources/views/components/generateForm/index.blade.php
@@ -27,7 +27,7 @@
             <div class="py-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <h2 class="font-semibold text-xl text-gray-800 leading-tight mb-8">
-                        TALL Crud Generator
+                        TALL Crud Gssssssduenerator
                     </h2>
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 bg-white border-b border-gray-200">
@@ -38,7 +38,6 @@
             </div>
         </main>
     </div>
-
     @livewireScripts
 </body>
 
