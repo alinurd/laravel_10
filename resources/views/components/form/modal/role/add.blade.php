d3e53dcfba5793aecbe60e174792448484141699
ali dev14 <ali.nrdn14005@gmail.com>
2024-11-12 11:10:07 +0700

    ok

--- resources/views/components/form/modal/role/add.blade.php
+++ resources/views/components/form/modal/role/add.blade.php
@@ -44,31 +44,30 @@
           <tr>
 
             <th>Menu</th>
-            <th>Index</th>
+            <th>Manage</th>
             <th>Create</th>
             <th>Delete</th>
             <th>Update</th>
-            <th>Print</th>
+            <th>View</th>
           </tr>
         </thead>
         <tbody>
           @foreach ($menuGroup as $g)
           <tr>
             <td><strong>{{ $g->name }}</strong></td>
-            <td><input type="checkbox" class="form-switch" name="index[{{ $g->id }}][]" id="index_{{ $g->id }}"></td>
-            <td><input type="checkbox" class="form-switch" name="create[{{ $g->id }}][]" id="create_{{ $g->id }}"></td>
-            <td><input type="checkbox" class="form-switch" name="delete[{{ $g->id }}][]" id="delete_{{ $g->id }}"></td>
-            <td><input type="checkbox" class="form-switch" name="update[{{ $g->id }}][]" id="update_{{ $g->id }}"></td>
-            <td><input type="checkbox" class="form-switch" name="print[{{ $g->id }}][]" id="print_{{ $g->id }}"></td>
+            <td><input type="checkbox" class="form-switch" name="manage[{{ $g->id }}][]" id="manage_{{ $g->id }}"></td>
+            <td></td>
+            <td></td>
+            <td></td>
           </tr>
           @foreach ($g->menuItems as $i)
           <tr>
             <td style="padding-left: 30px;"><strong>{{ $i->name }}</strong></td>
-            <td><input type="checkbox" class="form-switch" name="index[{{ $g->id }}][{{ $i->id }}]" id="index_{{ $g->id }}_{{ $i->id }}" data-bs-toggle="tooltip" data-bs-placement="top" title="access to {{ $i->name }}"></td>
+            <td><input type="checkbox" class="form-switch" name="manage[{{ $g->id }}][{{ $i->id }}]" id="manage_{{ $g->id }}_{{ $i->id }}" data-bs-toggle="tooltip" data-bs-placement="top" title="Manage  {{ $i->name }}"></td>
             <td><input type="checkbox" class="form-switch" name="create[{{ $g->id }}][{{ $i->id }}]" id="create_{{ $g->id }}_{{ $i->id }}" data-bs-toggle="tooltip" data-bs-placement="top" title="create data {{ $i->name }}"></td>
             <td><input type="checkbox" class="form-switch" name="delete[{{ $g->id }}][{{ $i->id }}]" id="delete_{{ $g->id }}_{{ $i->id }}" data-bs-toggle="tooltip" data-bs-placement="top" title="delete data {{ $i->name }}"></td>
             <td><input type="checkbox" class="form-switch" name="update[{{ $g->id }}][{{ $i->id }}]" id="update_{{ $g->id }}_{{ $i->id }}" data-bs-toggle="tooltip" data-bs-placement="top" title="update data {{ $i->name }}"></td>
-            <td><input type="checkbox" class="form-switch" name="print[{{ $g->id }}][{{ $i->id }}]" id="print_{{ $g->id }}_{{ $i->id }}" data-bs-toggle="tooltip" data-bs-placement="top" title="print data {{ $i->name }}"></td>
+            <td><input type="checkbox" class="form-switch" name="view[{{ $g->id }}][{{ $i->id }}]" id="view_{{ $g->id }}_{{ $i->id }}" data-bs-toggle="tooltip" data-bs-placement="top" title="view data {{ $i->name }}"></td>
           </tr>
           @endforeach
           @endforeach
