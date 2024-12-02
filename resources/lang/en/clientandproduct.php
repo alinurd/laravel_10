993b6bb4c9d85ea1053ae78c375d8ad7de73b097
ali dev14 <ali.nrdn14005@gmail.com>
2024-12-02 14:19:12 +0700

    add client product

---
+++ resources/lang/en/clientandproduct.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'title' => 'Data Clinet & Product',
+    'page' => 'Data Client & Product',
+    'pt_id' => 'PT',
+    'pic_id' => 'PIC',
+    'direktur' => 'Direktur',
+    'product' => 'Product',
+    'jenis' => 'Jenis Product',
+    'spesifikasi' => 'Spesifkasi Product',
+    'sut' => 'SUT',
+    'merk' => 'Merk',
+    'code_hs' => 'HS Code Product Jadi',
+    'status' => 'Status',
+];
