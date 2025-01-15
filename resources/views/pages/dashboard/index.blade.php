c09e1922fdf244ee8b005b0cb1c10069c521feac
ali dev14 <ali.nrdn14005@gmail.com>
2025-01-16 01:08:18 +0700

    started dashboard

---
+++ resources/views/pages/dashboard/index.blade.php
@@ -0,0 +1,89 @@
+@extends('components.layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('breadcrumb')
+<x-dashboard.breadcrumb
+    :title="'Dashboard'"
+    :page="'Dashboard'"
+    :active="'Dashboard'"
+    :route="route('dashboard.index')" />
+@endsection
+@section('content')
+<div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Total Sertifikat Tahunan</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="col">
+                        <div class="card bg-primary text-white">
+                            <div class="card-body ">
+                                <span class="card-title">Total Sertifikat</span>
+                                <center>
+                                    <h3 class="card-text text-white"><strong>100</strong></h3>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card bg-success text-white">
+                            <div class="card-body">
+                                <span class="card-title">Total Approved</span>
+                                <center>
+                                    <h3 class="card-text text-white"><strong>100</strong></h3>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card bg-warning text-white">
+                            <div class="card-body">
+                                <span class="card-title">Total Review</span>
+                                <center>
+                                    <h3 class="card-text text-white"><strong>100</strong></h3>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card bg-danger text-white">
+                            <div class="card-body">
+                                <span class="card-title">Total Reject</span>
+                                <center>
+                                    <h3 class="card-text text-white"><strong>100</strong></h3>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Total Nilai Barang 2024</h5>
+                            <center>
+                                <h3 class="card-text"><strong>25.000.000</strong></h3>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Total Nominal Sertifikat Tahunan</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        </div>
+    </div>
+</div>
+@endsection
