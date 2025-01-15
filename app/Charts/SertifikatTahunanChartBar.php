58346c6abd635943bd5508d4f7fae6daee2d2a2c
ali dev14 <ali.nrdn14005@gmail.com>
2025-01-16 02:12:54 +0700

    ok

--- app/Charts/SertifikatTahunanChartBar.php
+++ app/Charts/SertifikatTahunanChartBar.php
@@ -15,11 +15,10 @@ class SertifikatTahunanChartBar
 
     public function build(): \ArielMejiaDev\LarapexCharts\BarChart
     {
-        return $this->chart->barChart()
-            ->setTitle('San Francisco vs Boston.')
-            ->setSubtitle('Wins during season 2021.')
-            ->addData('San Francisco', [6, 9, 3, 4, 10, 8])
-            ->addData('Boston', [7, 3, 8, 2, 6, 4])
+        return $this->chart->barChart() 
+            ->addData('Approved', [6, 9, 3, 4, 10, 8])
+            ->addData('Reject', [7, 3, 8, 2, 6, 4])
+            ->setColors(['#0ab39c', '#f06548'])
             ->setXAxis(['January', 'February', 'March', 'April', 'May', 'June']);
     }
 }
