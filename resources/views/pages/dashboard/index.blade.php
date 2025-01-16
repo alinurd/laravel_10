3ad51bf733f10b311363e11cadb14f3f4049bd8c
ali dev14 <ali.nrdn14005@gmail.com>
2025-01-16 14:50:54 +0700

    ok

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -20,59 +20,61 @@
 <div class="card card-height-100 ">
     <div class="card-body">
         <div class="row row-cols-1 row-cols-md-2 g-4">
-            <div class="col">
-                <select name="" id="" class="select2 form-control">
-                    <option value="">-Pilih Tahun-</option>
+
+            <div class="input-group">
+                <select class="form-select" id="inputGroupSelect04" aria-label="Example select2  with button addon">
+                    <option selected>Choose...</option>
                     <option value="">2025</option>
                     <option value="">2024</option>
                     <option value="">2023</option>
                 </select>
+                <button class="btn btn-outline-primary" type="button">Search</button>
             </div>
-            <button class="btn btn-primary">Search</button>
+
         </div>
     </div>
-    </div>
+</div>
 
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Total Sertifikat Tahunan</h5>
-                    <div class="p-6 m-20 bg-white rounded shadow">
-        {!! $chartBar->container() !!}
-    </div>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Total Sertifikat Tahunan</h5>
+                <div class="p-6 m-20 bg-white rounded shadow">
+                    {!! $chartBar->container() !!}
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row row-cols-1 row-cols-md-2 g-4">
-                        <div class="col">
-                            <div class="card bg-primary text-white">
-                                <div class="card-body ">
-                                    <span class="card-title">Total Sertifikat</span>
-                                    <center>
-                                        <h3 class="card-text text-white"><strong>{{$totalSerti}}</strong></h3>
-                                    </center>
-                                </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="col">
+                        <div class="card bg-primary text-white">
+                            <div class="card-body ">
+                                <span class="card-title">Total Sertifikat</span>
+                                <center>
+                                    <h3 class="card-text text-white"><strong>{{$totalSerti}}</strong></h3>
+                                </center>
                             </div>
                         </div>
-                        <div class="col">
-                            <div class="card bg-success text-white">
-                                <div class="card-body">
-                                    <span class="card-title">Total Approved</span>
-                                    <center>
+                    </div>
+                    <div class="col">
+                        <div class="card bg-success text-white">
+                            <div class="card-body">
+                                <span class="card-title">Total Approved</span>
+                                <center>
                                     <h3 class="card-text text-white"><strong>{{$totalApprv}}</strong></h3>
-                                    </center>
-                                </div>
+                                </center>
                             </div>
                         </div>
-                        <div class="col">
-                            <div class="card bg-warning text-white">
-                                <div class="card-body">
-                                    <span class="card-title">Total Review</span>
-                                    <center>
+                    </div>
+                    <div class="col">
+                        <div class="card bg-warning text-white">
+                            <div class="card-body">
+                                <span class="card-title">Total Review</span>
+                                <center>
                                     <h3 class="card-text text-white"><strong>{{$totalReview}}</strong></h3>
                                 </center>
                             </div>
@@ -83,33 +85,33 @@
                             <div class="card-body">
                                 <span class="card-title">Total Reject</span>
                                 <center>
-                                        <h3 class="card-text text-white"><strong>{{$totalReject}}</strong></h3>
-                                     </center>
-                                </div>
+                                    <h3 class="card-text text-white"><strong>{{$totalReject}}</strong></h3>
+                                </center>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Total Nilai Barang 2024</h5>
-                                <center>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Total Nilai Barang 2024</h5>
+                            <center>
                                 <h2 class="card-text"><strong>Rp. {{ number_format($totalNilaiProduct, 2, ',', '.') }}</strong></h2>
-                                 </center>
-                            </div>
+                            </center>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Total Nominal Sertifikat Tahunan</h5>
-                {!! $chartLine->container() !!}
+</div>
+<div class="col">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Total Nominal Sertifikat Tahunan</h5>
+            {!! $chartLine->container() !!}
 
-            </div>
         </div>
     </div>
-    @endsection
+</div>
+@endsection
