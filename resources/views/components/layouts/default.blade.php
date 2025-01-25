f8fb4e82b463a7794b3e15010abdf2dc768d7fc4
ali dev14 <ali.nrdn14005@gmail.com>
2025-01-25 17:53:03 +0700

    pisahin footer

--- resources/views/components/layouts/default.blade.php
+++ resources/views/components/layouts/default.blade.php
@@ -65,23 +65,8 @@
       </div>
       <!-- End Page-content -->
 
-      <footer class="footer">
-        <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6">
-            ©
-              <script>
-                document.write(new Date().getFullYear())
-              </script> 
-            </div>
-            <div class="col-sm-6">
-              <div class="text-sm-end d-none d-sm-block">
-                Design & Develop by AliNurdin
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
+      @include('components.layouts.footer')
+
     </div>
     <!-- end main content-->
 
