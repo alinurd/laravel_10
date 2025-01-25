47abc621a9e845cf2730ef1961ea531a825f871a
ali dev14 <ali.nrdn14005@gmail.com>
2025-01-25 17:49:39 +0700

    ok

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -102,13 +102,14 @@
         <div class="container-fluid">
           <div class="row">
             <div class="col-sm-6">
+            ©
               <script>
                 document.write(new Date().getFullYear())
-              </script> © Velzon.
+              </script> 
             </div>
             <div class="col-sm-6">
               <div class="text-sm-end d-none d-sm-block">
-                Design & Develop by Themesbrand
+                Design & Develop by AliNurdin
               </div>
             </div>
           </div>
--- resources/views/components/layouts/default.blade.php
+++ resources/views/components/layouts/default.blade.php
@@ -67,15 +67,16 @@
 
       <footer class="footer">
         <div class="container-fluid">
-          <div class="row">
+        <div class="row">
             <div class="col-sm-6">
+            ©
               <script>
                 document.write(new Date().getFullYear())
-              </script> © Velzon.
+              </script> 
             </div>
             <div class="col-sm-6">
               <div class="text-sm-end d-none d-sm-block">
-                Design & Develop by Themesbrand
+                Design & Develop by AliNurdin
               </div>
             </div>
           </div>
