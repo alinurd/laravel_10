cd700ce970c08fd0cbc1743404c513d573888acb
ali dev14 <ali.nrdn14005@gmail.com>
2025-03-24 01:26:35 +0700

    add kategori ok

--- app/Http/Controllers/kategoriController.php
+++ app/Http/Controllers/kategoriController.php
@@ -17,22 +17,14 @@ class kategoriController extends _Controller
     protected $modelMaster = '';
     public function __construct()
     {
-        $this->modulName = "docferify";
-        $this->modelMaster = "App\Models\Combo";
+        $this->modulName = "kategori";
+        $this->modelMaster = "App\Models\Kategori";
         $option = [
             ['id' => 1, 'value' => 'Active'],
             ['id' => 2, 'value' => 'Non Acive'],
         ];
-        $categori = [
-            ['id' => "docferify", 'value' => 'Document Verify'], 
-        ];
-        $key1 = [];
-        foreach (range('A', 'F') as $letter) {
-            $key1[] = [
-                'id' => $letter,
-                'value' => $letter,
-            ];
-        }
+        
+        
         $this->list = [
             [
                 'field' => 'id',
@@ -44,7 +36,7 @@ class kategoriController extends _Controller
                 'rules' => ['required', 'string']
             ], 
             [
-                'field' => 'data',
+                'field' => 'nama',
                 'type' => 'text',
                 'filter' => false,
                 'position' => false,
@@ -53,36 +45,12 @@ class kategoriController extends _Controller
                 'rules' => ['required', 'string']
             ],
             [
-                'field' => 'key1',
-                'type' => 'select',
-                'filter' => true,
-                'position' => 'center',
-                'show' => true,
-                'showList' => true,
-                'required' => true,
-                'where' => null,
-                'option' => $key1,
-                'multiple' => false,
-            ],
-            [
-                'field' => 'categori',
-                'type' => 'select',
-                'filter' => true,
-                'position' => 'center',
-                'show' => true,
-                'showList' => false,
-                'required' => true,
-                'where' => null,
-                'option' => $categori,
-                'multiple' => false,
-            ],
-            [
                 'field' => 'status',
                 'type' => 'radio',
                 'filter' => true,
                 'position' => 'center',
                 'show' => true,
-                'required' => false,
+                'required' => true,
                 'where' => null,
                 'option' => $option,
                 'multiple' => false,
@@ -97,11 +65,8 @@ class kategoriController extends _Controller
     public function index()
     {
         $data = $this->_SETCORE;
-        $data['list'] = array_merge($this->setFrom);
-        $where=[
-            'where'=>['field'=>'categori', 'where'=>'kategori']
-        ];
-        $data['field'] = $this->getCombo($this->modelMaster,$where);
+        $data['list'] = array_merge($this->setFrom); 
+        $data['field'] = $this->getCombo($this->modelMaster,false);
         $data['sessionOK'] = session('success');
          return view('pages.index', $data);
     }
