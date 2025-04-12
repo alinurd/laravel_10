cd8f5354dbea26bc3d2e870a5009b23cd9677886
ali dev14 <ali.nrdn14005@gmail.com>
2025-04-12 12:55:00 +0700

    ok

--- resources/lang/en/documenctferify.php
+++ resources/lang/en/documenctferify.php
@@ -3,6 +3,7 @@
 return [
     'title' => 'Document Verify',
     'page' => 'Document Verify',
+    'fld_kode' => 'Kode DF',
     'fld_pic' => 'Nama Barang Penyedia/Jasa',
     'fld_jenis_product' => 'Jenis Product Yang Dinilai',
     'fld_nilai' => 'Nilai Product',
--- resources/lang/en/documenctferifyreview.php
+++ resources/lang/en/documenctferifyreview.php
@@ -4,6 +4,7 @@ return [
     'title' => 'Document Review',
     'page' => 'Document Review',
     'fld_pic' => 'Nama Barang Penyedia/Jasa',
+    'fld_kode' => 'Kode DF',
     'fld_jenis_product' => 'Jenis Product Yang Dinilai',
     'fld_nilai' => 'Nilai Product',
 ];
