7475a757a6b9a668418b08bd33d58aa860a34858
ali dev14 <ali.nrdn14005@gmail.com>
2025-04-19 15:40:35 +0700

    ok

--- resources/views/components/form/modal/integrasi/show.blade.php
+++ resources/views/components/form/modal/integrasi/show.blade.php
@@ -3,7 +3,7 @@
   <div class="modal-dialog modal-fullscreen">
     <div class="modal-content">
       <div class="modal-header bg-light">
-        <h5 class="modal-title text-primary">Detail Dokumen</h5>
+        <h5 class="modal-title text-primary"></h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body p-4">
