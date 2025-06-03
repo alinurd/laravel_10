a9844063758f65053ac6be924a3d86b07bec2f37
alinurd <ali.mnrdn14005@gamail.com>
2025-06-03 14:40:43 +0700

    ok

--- app/Models/SawModel.php
+++ app/Models/SawModel.php
@@ -11,36 +11,37 @@ class SawModel
      * @param array $jawaban => list nilai jawaban (alternatif_id, kriteria_id, nilai)
      * @return array
      */
-    public static function hitungRanking(array $alternatif, array $kriteria, array $jawaban, string $user=null): array
+    public static function hitungRanking(array $alternatif, array $kriteria, array $jawaban, string $user = null): array
     {
-        
+
         $pembagiKriteria = [];
         foreach ($kriteria as $k) {
             $nilai = collect($jawaban)->where('kriteria_id', $k['id'])->pluck('nilai')->filter()->all();
             $pembagiKriteria[$k['id']] = !empty($nilai)
-                ? ($k['atribut'] == 1 ? min($nilai) : max($nilai))
+                ? ($k['atribut'] == 1 ? 1 : 3)
+                // ? ($k['atribut'] == 1 ? min($nilai) : max($nilai))
                 : 1;
         }
- 
+
         $hasil = [];
         foreach ($alternatif as $alt) {
             $skor = 0;
             foreach ($kriteria as $k) {
-                $nilai = collect($jawaban)->first(fn ($j) => $j['chanel_id'] == $alt['id'] && $j['kriteria_id'] == $k['id'])['nilai'] ?? 0;
+                $nilai = collect($jawaban)->first(fn($j) => $j['chanel_id'] == $alt['id'] && $j['kriteria_id'] == $k['id'])['nilai'] ?? 0;
                 // $normalisasi = $k['atribut'] == 1
                 //     ? ($pembagiKriteria[$k['id']] != 0 ? $pembagiKriteria[$k['id']] / $nilai : 0)
                 //     : ($pembagiKriteria[$k['id']] != 0 ? $nilai / $pembagiKriteria[$k['id']] : 0);
                 $normalisasi = 0;
 
-if ($k['atribut'] == 1) { // cost
-    if ($nilai != 0) {
-        $normalisasi = $pembagiKriteria[$k['id']] / $nilai;
-    }
-} else { // benefit
-    if ($pembagiKriteria[$k['id']] != 0) {
-        $normalisasi = $nilai / $pembagiKriteria[$k['id']];
-    }
-}
+                if ($k['atribut'] == 1) { // cost
+                    if ($nilai != 0) {
+                        $normalisasi = $pembagiKriteria[$k['id']] / $nilai;
+                    }
+                } else { // benefit
+                    if ($pembagiKriteria[$k['id']] != 0) {
+                        $normalisasi = $nilai / $pembagiKriteria[$k['id']];
+                    }
+                }
 
 
                 $skor += $normalisasi * $k['bobot_normalisasi'];
@@ -51,7 +52,7 @@ if ($k['atribut'] == 1) { // cost
             ];
         }
 
-         
+
         return collect($hasil)->sortByDesc('skor')->values()->all();
     }
 }
